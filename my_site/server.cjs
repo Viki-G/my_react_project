3b730562b0d065e85d56cf2c32484ea0f7c5b110
Viki-G <vgugovska@abv.bg>
2025-05-23 08:32:58 +0300

    Added a favicon to the site
    Added a favicon

--- my_site/server.cjs
+++ my_site/server.cjs
@@ -17,7 +17,7 @@ app.use(cors({
 const filePath = path.join(__dirname, 'animation.json');
 console.log(`Looking for file at: ${filePath}`);
 
-// API route to send the animation data
+
 app.get('/animation', (req, res) => {
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
@@ -25,11 +25,11 @@ app.get('/animation', (req, res) => {
       return res.status(500).send('Error reading file');
     }
     res.setHeader('Content-Type', 'application/json');
-    res.send(data); // Sending the JSON content
+    res.send(data); 
   });
 });
 
-// Start the server on port 3002
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
